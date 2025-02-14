b5bab362a0e75cbb54f0bd117fa2231b2f83de12
Nguyễn Lâm Nhật <zznhatphilong@gmail.com>
2025-02-14 20:22:17 +0700

    done UI view post list

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -4,3 +4,4 @@ export * from '@/app/types/pagination.type';
 export * from '@/app/types/search.type';
 export * from '@/app/types/dialog.type';
 export * from '@/app/types/badge.type';
+export * from '@/app/types/post.type';
---
+++ src/app/types/post.type.ts
@@ -0,0 +1,8 @@
+export interface Post {
+  id: string;
+  userId: string;
+  hashtag: string;
+  content: string;
+  mediaUrl: string;
+  isActive: boolean;
+}
